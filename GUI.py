17f49cf1cbf1aa0f22d14351ad7d24e4b3225620
frank400 <j.victorhuguenin2018@gmail.com>
2020-02-18 20:56:05 -0300

    insert OOP in the project

--- GUI.py
+++ GUI.py
@@ -12,77 +12,87 @@ Label(root, text='Amount:').grid(row=1, column=0, padx=1)
 
 
 # get the data from get_data file
-def USD_BRL():
-    value = entry.get()
-    amount = float(value) * float(get_data.get_USD_BRL())
-    entry.delete(0, END)
-    entry.insert(0, str(amount))
+class Stocks:
+    def USD_BRL(self):
+        cotation = float(get_data.get_USD_BRL())
+        return cotation
+
+    def BTC_USD(self):
+        cotation = float(get_data.get_BTC_USD())
+        return cotation
+
+    def EUR_BRL(self):
+        cotation = float(get_data.get_EUR_BRL())
+        return cotation
 
 
-def BTC_USD():
-    value = entry.get()
-    amount = float(value) * float(get_data.get_BTC_USD())
-    entry.delete(0, END)
-    entry.insert(0, str(amount))
+class Numpad(Stocks):
 
+    # calculate the value and displays it
+    def set_USD_BRL(self):
+        value = float(entry.get())
+        amount = value * super().USD_BRL()
+        entry.delete(0, END)
+        entry.insert(0, str(amount))
 
-def EUR_BRL():
-    value = entry.get()
-    amount = float(value) * float(get_data.get_EUR_BRL())
-    entry.delete(0, END)
-    entry.insert(0, str(amount))
+    def set_BTC_USD(self):
+        value = float(entry.get())
+        amount = value * super().BTC_USD()
+        entry.delete(0, END)
+        entry.insert(0, str(amount))
 
+    def set_EUR_BRL(self):
+        value = float(entry.get())
+        amount = value * super().EUR_BRL()
+        entry.delete(0, END)
+        entry.insert(0, str(amount))
 
-if __name__ == '__main__':
-    # creates 3 buttons to choose which currency we want
-    # USD->BRL
-    USD_BRL_button = Button(root, text='USD/BRL', padx=40,
-                            command=USD_BRL).grid(row=0, column=0)
-    # BTC->USD
-    BTC_USD_button = Button(root, text='BTC/USD', padx=40,
-                            command=BTC_USD).grid(row=0, column=1)
-    # EUR->BRL
-    EUR_BRL_button = Button(root, text='EUR/BRL', padx=40,
-                            command=EUR_BRL).grid(row=0, column=2)
-
-    # **numpad buttons**
-
-    def button_click(num):
+    # buttons functions
+    def button_click(self, num):
         current = entry.get()
         entry.delete(0, END)
         entry.insert(0, str(current)+str(num))
 
-    def button_clear():
+    def button_clear(self):
         entry.delete(0, END)
 
-    def button_delete():
+    def button_delete(self):
         entry.delete(0, END)
+
+    def set_button(self, num):
+        return Button(root, text=f'{num}', padx=70, pady=20, command=lambda: numpad.button_click(num))
+
+
+if __name__ == '__main__':
+    numpad = Numpad()
+    # creates 3 buttons to choose which currency we want
+    # USD->BRL
+    USD_BRL_button = Button(root, text='USD/BRL', padx=40,
+                            command=numpad.set_USD_BRL).grid(row=0, column=0)
+    # BTC->USD
+    BTC_USD_button = Button(root, text='BTC/USD', padx=40,
+                            command=numpad.set_BTC_USD).grid(row=0, column=1)
+    # EUR->BRL
+    EUR_BRL_button = Button(root, text='EUR/BRL', padx=40,
+                            command=numpad.set_EUR_BRL).grid(row=0, column=2)
+
+    # **numpad buttons**
 
     # define button
-    numpad_1 = Button(root, text='1', padx=70, pady=20,
-                      command=lambda: button_click(1))
-    numpad_2 = Button(root, text='2', padx=70, pady=20,
-                      command=lambda: button_click(2))
-    numpad_3 = Button(root, text='3', padx=70, pady=20,
-                      command=lambda: button_click(3))
-    numpad_4 = Button(root, text='4', padx=70, pady=20,
-                      command=lambda: button_click(4))
-    numpad_5 = Button(root, text='5', padx=70, pady=20,
-                      command=lambda: button_click(5))
-    numpad_6 = Button(root, text='6', padx=70, pady=20,
-                      command=lambda: button_click(6))
-    numpad_7 = Button(root, text='7', padx=70, pady=20,
-                      command=lambda: button_click(7))
-    numpad_8 = Button(root, text='8', padx=70, pady=20,
-                      command=lambda: button_click(8))
-    numpad_9 = Button(root, text='9', padx=70, pady=20,
-                      command=lambda: button_click(9))
-    numpad_0 = Button(root, text='0', padx=70, pady=20,
-                      command=lambda: button_click(0))
+    numpad_1 = numpad.set_button(1)
+    numpad_2 = numpad.set_button(2)
+    numpad_3 = numpad.set_button(3)
+    numpad_4 = numpad.set_button(4)
+    numpad_5 = numpad.set_button(5)
+    numpad_6 = numpad.set_button(6)
+    numpad_7 = numpad.set_button(7)
+    numpad_8 = numpad.set_button(8)
+    numpad_9 = numpad.set_button(9)
+    numpad_0 = numpad.set_button(0)
     numpad_clear = Button(root, text='Clear', padx=57,
-                          pady=20, command=button_clear)
+                          pady=20, command=numpad.button_clear)
     numpad_delete = Button(root, text='Delete', padx=54,
-                           pady=20, command=button_delete)
+                           pady=20, command=numpad.button_delete)
 
     # numpad grid
 
